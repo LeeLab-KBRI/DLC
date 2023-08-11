31d88bedca4ac9997faa69d4a37258dc22417316
LeeLab-KBRI <30518353+LeeLab-KBRI@users.noreply.github.com>
2023-08-11 11:26:24 +0900

    Create export_DLC.py

---
+++ export_DLC.py
@@ -0,0 +1,2 @@
+config_path = ' /home/computername/DeepLabCut2.0/yourprojectname'
+deeplabcut.export_model(cfg_path, iteration=None, shuffle=1, trainingsetindex=0, snapshotindex=None, TFGPUinference=True, overwrite=False, make_tar=True)
